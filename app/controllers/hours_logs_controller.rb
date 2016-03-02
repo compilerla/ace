b16b183373620751bdd3982a7aeb1432fbd99409
Stephen Corwin <corwinstephen@gmail.com>
2016-03-01 18:09:07 -0800

    Setting up data processor

--- app/controllers/hours_logs_controller.rb
+++ app/controllers/hours_logs_controller.rb
@@ -1,10 +1,32 @@
 class HoursLogsController < ApplicationController
   def create
-    flash[:notice] = "Yay"
+    if process_hours_data
+      flash[:notice] = 'Success'
+    else
+      flash[:danger] = 'Failure'
+    end
     redirect_to root_path
   end
 
   def create_row
 
+  end
+
+  private
+
+  def process_hours_data
+    HoursLog::TYPES.each do |log_type|
+      if params['typeweeks'][log_type].present?
+        return false unless valid_type_week?(params[log_type])
+      end
+
+    end
+
+    return true
+  end
+
+  def valid_type_week?(type_week_params)
+    # Todo
+    true
   end
 end
