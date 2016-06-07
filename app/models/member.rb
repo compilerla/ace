738bed8b9ce713b02b147d6cb74dde46f7ba40e1
Stephen Corwin <corwinstephen@gmail.com>
2016-06-06 17:32:18 -0700

    More specifics

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,7 +7,7 @@ class Member
   string :email
   string :id, identity: true
 
-  has_many :project_member
+  has_many :project_member, source_key: :member_id
 
   def self.find_by_email(email)
     where(email: email.gsub('@', '\@')).first
--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -16,18 +16,21 @@ class ProjectMember
   belongs_to :member
 
   def submission_periods_for_user(user)
-    term = user.terms.first
-
-    return [] unless term
-
-    intersect_start_date = [start_date, term.start_date].max
-    intersect_end_date = [end_date, term.end_date].min
-    iterator_date = intersect_start_date
-
     periods = []
-    while(iterator_date < intersect_end_date)
-      periods << SubmissionPeriod.new(iterator_date)
-      iterator_date += 1.week
+
+    user.terms.each do |term|
+
+      return [] unless term
+
+      intersect_start_date = [start_date, term.start_date].max
+      intersect_end_date = [end_date, term.end_date].min
+      iterator_date = intersect_start_date
+
+      while(iterator_date <= intersect_end_date)
+        periods << SubmissionPeriod.new(iterator_date)
+        iterator_date += 1.week
+      end
+      
     end
 
     periods
