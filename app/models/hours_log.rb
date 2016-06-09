4b023e2e6e647ec0642de888bd839d696476aaa4
Stephen Corwin <corwinstephen@gmail.com>
2016-06-08 19:08:02 -0700

    Default lunch hours

--- app/models/hours_log.rb
+++ app/models/hours_log.rb
@@ -1,11 +1,14 @@
 class HoursLog < ActiveRecord::Base
-  TYPES = ['service', 'training', 'vsp', 'break', 'personal_appointment', 'lunch']
+  TYPES = ['service', 'training', 'pto', 'vsp', 'lunch']
+
+  validates :log_type,
+            inclusion: { in: TYPES }
 
   belongs_to :project
   belongs_to :user
 
-  def self.formatted_types
-    TYPES.map(&:humanize)
+  def self.types_for_select
+    TYPES.map{ |type| [type.humanize, type] }
   end
 
   def submission_period
