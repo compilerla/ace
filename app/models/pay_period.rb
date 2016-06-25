fdf33c6609982b03b81249ad68179c64b985f99e
Stephen Corwin <corwinstephen@gmail.com>
2016-06-24 18:13:40 -0700

    Items sorted by period

--- app/models/pay_period.rb
+++ app/models/pay_period.rb
@@ -4,4 +4,12 @@ class PayPeriod < ActiveRecord::Base
                         :end_at,
                         :timesheet_due_at,
                         :pay_day_at
+
+  def self.through_today
+    where("end_at < ?", Date.today).order("end_at DESC")
+  end
+
+  def submissions
+    Submission.joins(:hours_logs).where("hours_logs.date > ? AND hours_logs.date < ?", start_at, end_at).uniq
+  end
 end
